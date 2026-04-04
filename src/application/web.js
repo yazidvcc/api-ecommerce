2f8c4213a8c5662c24026d24fd1bc3560bf16836
Yazidvcc <yazidkhairuloff@gmail.com>
2026-04-05 06:48:59 +0700

    fixed parent routes add protected router

--- src/application/web.js
+++ src/application/web.js
@@ -2,15 +2,15 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import publicRouter from '../route/public-api.js';
 import errorMiddleware from '../middleware/error-middleware.js';
-import userRouter from '../route/protected-api.js';
 import expressFileUpload from 'express-fileupload'
+import protectedRouter from '../route/protected-api.js';
 
 const web = express();
 web.use(express.json());
 web.use(expressFileUpload())
 web.use(cookieParser());
 web.use(publicRouter);
-web.use(userRouter);
+web.use(protectedRouter);
 web.use(errorMiddleware)
 
 export {
