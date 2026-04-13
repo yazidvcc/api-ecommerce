a0cd78eff5ce668f129fa10639a5c0ac5855aa4d
Yazidvcc <yazidkhairuloff@gmail.com>
2026-04-13 17:36:31 +0700

    add column gender in table product and validation gender on create and update product

--- src/validation/product-validation.js
+++ src/validation/product-validation.js
@@ -2,6 +2,7 @@ import Joi from "joi"
 
 const createProductValidation = Joi.object({
     name: Joi.string().max(100).required(),
+    gender: Joi.string().valid("MALE", "FEMALE", "UNISEX").required(),
     description: Joi.string().required(),
     category_id: Joi.number().required(),
     product_variants: Joi.array().items(Joi.object({
@@ -15,8 +16,16 @@ const createProductValidation = Joi.object({
 const updateProductValidation = Joi.object({
     id: Joi.number().positive().required(),
     name: Joi.string().max(100).required(),
+    gender: Joi.string().valid("MALE", "FEMALE", "UNISEX").required(),
     description: Joi.string().required(),
     category_id: Joi.number().required()
+})
+
+const searchProductValidation = Joi.object({
+    category_id: Joi.number().positive().optional(),
+    name: Joi.string().optional(),
+    page: Joi.number().min(1).optional(),
+    size: Joi.number().min(1).max(100).optional()
 })
 
 const idProductValidation = Joi.number().positive().required()
