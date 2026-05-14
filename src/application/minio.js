cef768182cc2d42d14c680d49cc3956f288fe334
Yazidvcc <yazidkhairuloff@gmail.com>
2026-05-14 10:35:54 +0700

    fixed bugs in setup bucket and error handles limit file upload

--- src/application/minio.js
+++ src/application/minio.js
@@ -18,6 +18,8 @@ async function initBucket() {
     }
 }
 
-await initBucket()
+(async () => {
+    await initBucket()
+})()
 
 export default minioClient
--- src/application/web.js
+++ src/application/web.js
@@ -12,6 +12,11 @@ web.use(expressFileUpload({
     limits: {
         fileSize: 2 * 1024 * 1024
     },
+    limitHandler: (req, res, next) => {
+        return res.status(413).json({
+            errors: 'File terlalu besar! Maksimal ukuran adalah 2MB.'
+        });
+    },
     abortOnLimit: true
 }))
 web.use(cookieParser());
